bf6740b21fde7abfbd6d7f029b23f93b0a232e02
Kai <sistason@sistason.de>
2020-03-15 12:57:13 +0100

    addded the proxyGrabber from torr_dl to show_dl

--- c_status_to_torrent/piratebay.py
+++ c_status_to_torrent/piratebay.py
@@ -1,5 +1,6 @@
 import asyncio
 import aiohttp
+import requests
 import logging
 import bs4
 import re
@@ -23,7 +24,7 @@ class PirateBayResult:
 
 
 class PiratebayGrabber:
-    url = 'https://thepiratebay.org'
+    proxy_url = "http://proxybay.one"
 
     def __init__(self, event_loop):
         self.event_loop = event_loop
@@ -31,6 +32,14 @@ class PiratebayGrabber:
         self.max_simultaneous_requests = asyncio.Semaphore(10)
 
         self.parser = PirateBayParser()
+        self.proxies = self.setup_proxies()
+
+    def setup_proxies(self):
+        ret = requests.get(self.proxy_url)
+        if ret.ok:
+            bs4_response = bs4.BeautifulSoup(ret.text, "lxml")
+            proxylist = bs4_response.find('table', attrs={'id': 'proxyList'})
+            return [p.td.a.attrs.get('href') for p in proxylist.find_all('tr') if p.td]
 
     async def close(self):
         if self.aiohttp_session is not None:
@@ -47,22 +56,23 @@ class PiratebayGrabber:
                         if r_.status == 200:
                             return text
                         else:
-                            logging.warning('{} returned status "{}", parser corrupt?'.format(self.url, r_.status))
+                            logging.warning('{} returned status "{}", parser corrupt?'.format(self.proxy_url, r_.status))
                 except (asyncio.TimeoutError, aiohttp.ClientConnectionError):
                     await asyncio.sleep(1)
                 except Exception as e:
                     logging.debug(
                         'Caught Exception "{}" while making a get-request to "{}"'.format(e.__class__, url))
                     return
-            logging.warning('Connection to {} failed. Site down?'.format(self.url))
+            logging.warning('Connection to {} failed. Site down?'.format(self.proxy_url))
 
     async def search(self, show, object_):
         query = "{} {}".format(show.get_search_query(), object_.str_short())
         query = re.sub(r'[^\w\d\s.]', '', query)
-        logging.debug('Searching piratebay for "{}"'.format(query))
-        response = await self._make_request(self.url + '/search/{}/0/99/200'.format(query))
-        if response:
-            return self.parser.parse_piratebay_response(response)
+        for proxy_url in self.proxies:
+            logging.debug('Searching {} for "{}"'.format(proxy_url, query))
+            response = await self._make_request(proxy_url + '/search/{}/0/99/200'.format(query))
+            if response:
+                return self.parser.parse_piratebay_response(response)
         return []
 
 
